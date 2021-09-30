3e8ce4cd6b1d6f6c34072b88bb4b3a48dbc27eb6
crispantufla <crispantufla@gmail.com>
2021-09-30 08:43:47 -0700

    innertxt

--- src/index.js
+++ src/index.js
@@ -15,33 +15,32 @@ console.log("websocket server created")
 const mongoose = require('mongoose');
 const messageModel = require('./models/message');
 
-
+const main = () => {
 
     mongoose.connect('mongodb+srv://admin:vA3WC3XFAs8nMWQPcw3PD@cluster0.9lknw.mongodb.net/chat?retryWrites=true&w=majority', { useUnifiedTopology: true ,useNewUrlParser: true })
         .then(db => console.log('db is connected')).catch(err => console.log(err))
 
     wss.broadcast = (data) => {
         wss.clients.forEach((client) => {
-            client.send(data);
+            client.send(JSON.stringify(data));
         });
     }
 
     wss.on('connection', async socket => {
         let messages = await messageModel.find().lean();
         if (messages.length > 0) {
-            messages.forEach((message) => {socket.send('<div class="author">' + message.author + '</div> <div class="msg-content">' + message.content + '</div>')})
+            messages.forEach((message) => {socket.send(JSON.stringify({author: message.author, content: message.content}))})
         }
 
         socket.on('message',  async msg => {
             let data = JSON.parse(msg);
-            let newMessage = new messageModel({
-                content: data.message,
-                author: data.name
-            });
-
-            if (data.message) {
-                newMessage.save();
-                wss.broadcast('<div class="author">' + data.name + '</div> <div class="msg-content">' + data.message + '</div>');
+            if (data.content) {
+                new messageModel({
+                    content: data.content,
+                    author: data.author
+                }).save();
+                
+                wss.broadcast(data);
             } 
         });
     })
@@ -49,3 +48,6 @@ const messageModel = require('./models/message');
     server.listen(PORT, () => {
         console.log(`server on port ${PORT}`)
     })
+}
+
+main()
