5118f4e03fb3d9edb48a6b661d153f44a1c1c956
crispantufla <crispantufla@gmail.com>
2021-09-29 20:14:35 -0700

    fix connection

--- src/index.js
+++ src/index.js
@@ -17,40 +17,47 @@ app.use(cors({
 
 app.use(express.static(path.join(__dirname, 'public')))
 
-mongoose.connect('mongodb+srv://admin:vA3WC3XFAs8nMWQPcw3PD@cluster0.9lknw.mongodb.net/chat?retryWrites=true&w=majority', { useUnifiedTopology: true ,useNewUrlParser: true })
-    .then(db => console.log('db is connected')).catch(err => console.log(err))
-
 const WebSocketServer = require('ws').Server;
 const wss = new WebSocketServer({
     port: 8080
 })
 
-wss.broadcast = (data) => {
-    wss.clients.forEach((client) => {
-        client.send(data);
-    });
-}
+const main = async () => {
 
-wss.on('connection', async socket => {
-    let messages = await messageModel.find().lean();
-    if (messages.length > 0) {
-        messages.forEach((message) => {socket.send('<div class="author">' + message.author + '</div> <div class="msg-content">' + message.content + '</div>')})
+    await mongoose.connect('mongodb+srv://admin:vA3WC3XFAs8nMWQPcw3PD@cluster0.9lknw.mongodb.net/chat?retryWrites=true&w=majority', { useUnifiedTopology: true ,useNewUrlParser: true })
+        .then(db => console.log('db is connected')).catch(err => console.log(err))
+
+
+
+    wss.broadcast = (data) => {
+        wss.clients.forEach((client) => {
+            client.send(data);
+        });
     }
 
-    socket.on('message',  async msg => {
-        let data = JSON.parse(msg);
-        let newMessage = new messageModel({
-            content: data.message,
-            author: data.name
+    wss.on('connection', async socket => {
+        let messages = await messageModel.find().lean();
+        if (messages.length > 0) {
+            messages.forEach((message) => {socket.send('<div class="author">' + message.author + '</div> <div class="msg-content">' + message.content + '</div>')})
+        }
+
+        socket.on('message',  async msg => {
+            let data = JSON.parse(msg);
+            let newMessage = new messageModel({
+                content: data.message,
+                author: data.name
+            });
+
+            if (data.message) {
+                newMessage.save();
+                wss.broadcast('<div class="author">' + data.name + '</div> <div class="msg-content">' + data.message + '</div>');
+            } 
         });
+    })
 
-        if (data.message) {
-            newMessage.save();
-            wss.broadcast('<div class="author">' + data.name + '</div> <div class="msg-content">' + data.message + '</div>');
-        } 
-    });
-})
+    http.listen(PORT, () => {
+        console.log(`server on port ${PORT}`)
+    })
+}
 
-http.listen(PORT, () => {
-    console.log(`server on port ${PORT}`)
-})
+main()
